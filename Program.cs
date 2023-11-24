b43dc77f57019af8f974d63c78e296caf7ae2a46
Stephan Kansy <stkansy@gmail.com>
2023-11-24 08:53:54 +0100

    Content negotiation in Web/REST API

--- Program.cs
+++ Program.cs
@@ -7,8 +7,12 @@ namespace Web_API_Tutorials_.Net_Core_7_C_
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Added new libs
-            builder.Services.AddControllers().AddNewtonsoftJson();
+            // Added new libs: builder.Services.AddControllers().AddNewtonsoftJson();
+
+            // added options: non supportet format request error: zb für xml obwohl nur json supportet wird
+            // added: .AddXmlDataContractSerializerFormatters() damit xml supportet wird
+            builder.Services.AddControllers(options => options.ReturnHttpNotAcceptable = true)
+            .AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
